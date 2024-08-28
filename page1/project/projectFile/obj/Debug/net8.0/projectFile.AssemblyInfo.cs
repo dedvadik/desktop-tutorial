31e0dcac521d61daba375936a9d9817b60ac1f72
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-08-28 12:25:45 +0300

    проверки

--- page1/project/projectFile/obj/Debug/net8.0/projectFile.AssemblyInfo.cs
+++ page1/project/projectFile/obj/Debug/net8.0/projectFile.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b1ff5315bdc46cd910cec4f767fc7ad660ee90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5230c8579869d61ef849f661cbe52c8fca9a6c31")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
