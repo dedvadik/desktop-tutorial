2e087c39c0d2b31a8de1706414ada0a8c45a6c0d
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-08-28 20:48:08 +0300

    clone for my PC

--- page1/project/projectFile/obj/Debug/net8.0/projectFile.AssemblyInfo.cs
+++ page1/project/projectFile/obj/Debug/net8.0/projectFile.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2392c24b6c09ec457b551106c623fc3938f7953c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bff4b84515ced3ae864879f72d829b8e8eacd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
