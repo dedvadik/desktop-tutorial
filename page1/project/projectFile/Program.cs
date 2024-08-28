b9d7f050937812628532e1129184c7073b2a167f
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-08-28 11:29:47 +0300

    проверка можно ли на русском делать комиты

--- page1/project/projectFile/Program.cs
+++ page1/project/projectFile/Program.cs
@@ -3,5 +3,6 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Проверка можно ли на русском делать комиты");
     }
 }
