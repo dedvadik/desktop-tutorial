06004c3d4d7d45df25cbb0d22ffabcdc33e90c7f
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-08-30 10:40:17 +0300

    удаление лишнего

--- page1/project/projectFile/NewCleanProg.cs
+++ page1/project/projectFile/NewCleanProg.cs
@@ -2,11 +2,19 @@ class NewCleanProg
 {
     public string Prog()
     {
+        /*
         string firstName = "Vadim";
         int widgsPurchased = 7;
         //Testing a change to the message.
         //int widgetsSold = 7;
         //return $"{firstName} purchased {widgetsSold} widgets.";
         return $"{firstName} purchased {widgsPurchased} widgets.";
+        */
+        Random random  = new Random();
+        string[] orderIDs = new string[];
+        for (int i =0; i < orderIDs.Length; i++)
+        {
+            int prefixValue = 
+        }
     }
 }
